b0f5db68333dba7f85667a84c5c82ed948985b43
Nathan Hemingway <nhemingway@gmail.com>
2019-03-18 14:59:40 -0700

    Remove Python 2 compat and added snapshot

--- upgrade_monitor.py
+++ upgrade_monitor.py
@@ -6,21 +6,20 @@ Licence: Apache
 This script is provided as is. No support is implied.
 
 """
-from __future__ import print_function
-from __future__ import with_statement
-from pprint import pformat
 from getpass import getpass
-import time
-import json
-import sys
-import os
+from pprint import pformat
 import argparse
+import json
+import os
+import sys
+import time
 import traceback
 # import websocket
 
-# if sys.version_info[0] < 3:
-#     print('This script requires python 3.')
-#     exit()
+try:
+    from typing import List, Dict, Union, Generic, TypeVar
+except ImportError:
+    print('This script requires python 3.')
 
 DEPS = ['requests', 'termcolor']
 
@@ -35,29 +34,19 @@ except ModuleNotFoundError:
         print(dep)
     exit()
 
-if hasattr(__builtins__, 'raw_input'):
-    input = __builtins__.raw_input
-
 urllib3.disable_warnings()
 
 
-def add1(x: int) -> int:
-    return x + 1
-
-
-add1("w00t")
-
-
-def inline_progress(message, percent):  # type: (str, int) -> None
+def inline_progress(message: str, percent: int) -> None:
     """Print progress inline, overwriting current line"""
     sys.stdout.write(message % percent + '\r')
     sys.stdout.flush()
 
 
-def logger(color):
+def logger(color: str):
     """Log results"""
 
-    def _logger(header, data, *args):
+    def _logger(header: str, data: Union[Dict, List, str], *args) -> None:
         if isinstance(data, dict) or isinstance(data, list):
             print(colored('\n[%s]\n' % header, color), pformat(data))
         else:
@@ -75,30 +64,24 @@ class AuthException(Exception):
     pass
 
 
-def Option(data):
-    """Create option type"""
-    if isinstance(data, Something):
-        return data
-    if data is not None:
-        return Something(data)
-    return Something(None)
+T = TypeVar('T')
 
 
-class Something(object):
+class Something(Generic[T]):
     """Option type wrapper for safe attribute access"""
 
-    def __init__(self, data):
+    def __init__(self, data: T):
         self.data = data
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         if self.data is None:
             return 'Nothing'
         return 'Something(%s)' % self.data.__repr__()
 
-    def __str__(self):
+    def __str__(self) -> str:
         return str(self.data)
 
-    def __getitem__(self, key):
+    def __getitem__(self, key: Union[str, int]):
         try:
             return Something(self.data[key])
         except (KeyError, TypeError):
@@ -110,23 +93,33 @@ class Something(object):
                 if x is not None:
                     yield Option(x)
 
-    def __nonzero__(self):
+    def __nonzero__(self) -> bool:
         return self.data is not None
 
-    def __bool__(self):
+    def __bool__(self) -> bool:
         return self.data is not None
 
-    def value(self):
+    def value(self) -> T:
         return self.data
+
+
+def Option(data) -> Something:
+    """Create option type"""
+    if isinstance(data, Something):
+        return data
+    if data is not None:
+        return Something(data)
+    return Something(None)
 
 
 class APIC(object):
     """APIC state data"""
 
-    def __init__(self, options):
+    def __init__(self, options: argparse.Namespace):
         self.options = options
         self.jar = None
         self.start_time = time.time()
+        self.faults = []  # type: List[str]
         self.last_refresh = 0
 
 
@@ -147,14 +140,14 @@ def request(apic, method, relative_url, data={}):
     return requests.get(url, cookies=apic.jar, verify=False, timeout=30)
 
 
-def get(apic, relative_url):  # type: (APIC, str) -> Something
+def get(apic: APIC, relative_url: str) -> Something:
     """Fetch and unwrap API request"""
     refresh_token(apic)
     res = request(apic, 'GET', relative_url)
     return Option(res.json())['imdata']
 
 
-def login(apic):  # type: (APIC) -> APIC
+def login(apic: APIC) -> APIC:
     """Login to the APIC"""
     res = request(
         apic, 'POST', '/api/aaaLogin', {
@@ -172,7 +165,7 @@ def login(apic):  # type: (APIC) -> APIC
     return apic
 
 
-def refresh_token(apic):  # type: (APIC) -> APIC
+def refresh_token(apic: APIC) -> APIC:
     """Check last token refresh and refresh if needed"""
     elapsed_time = time.time() - apic.last_refresh
     if elapsed_time > apic.options.token_refresh_interval:
@@ -182,7 +175,7 @@ def refresh_token(apic):  # type: (APIC) -> APIC
     return apic
 
 
-def get_options():  # type: () -> argparse.Namespace
+def get_options() -> argparse.Namespace:
     """Parse command line args."""
     DEFAULT_REQUEST_INTERVAL = 10
     DEFAULT_LOGIN_INTERVAL = 60
@@ -228,7 +221,7 @@ def get_options():  # type: () -> argparse.Namespace
     parser.add_argument('ip', help='APIC IP address')
     args = parser.parse_args()
     if not args.usr:
-        args.usr = input('Username: ')  # type: ignore
+        args.usr = input('Username: ')
     if not args.pwd:
         args.pwd = getpass('Password: ')
     if not args.ip:
@@ -236,13 +229,13 @@ def get_options():  # type: () -> argparse.Namespace
     return args
 
 
-def get_devices(apic):  # type: (APIC) -> list(Something)
+def get_devices(apic: APIC) -> List[Something]:
     """Get list of all devices on fabric."""
     devices = get(apic, '/api/class/topSystem')
     return [Option(d)['topSystem']['attributes'] for d in devices]
 
 
-def get_apic_status(apic, device):  # type: (APIC, list(Something)) -> dict
+def get_apic_status(apic: APIC, device: Something) -> Dict[str, Something]:
     """Get APIC upgrade status."""
     dn = device['dn'].value()
     url_job = '/api/mo/%s/ctrlrfwstatuscont/upgjob' % dn
@@ -252,7 +245,7 @@ def get_apic_status(apic, device):  # type: (APIC, list(Something)) -> dict
     return {'device': device, 'status': status, 'running': running}
 
 
-def get_switch_status(apic, device):  # type: (APIC, list(Something)) -> dict
+def get_switch_status(apic: APIC, device: Something) -> Dict[str, Something]:
     """Get switch upgrade status."""
     dn = device['dn'].value()
     url_job = '/api/mo/%s/fwstatuscont/upgjob' % dn
@@ -262,7 +255,8 @@ def get_switch_status(apic, device):  # type: (APIC, list(Something)) -> dict
     return {'device': device, 'status': status, 'running': running}
 
 
-def get_device_status(apic, devices):  # type (APIC, dict) -> dict
+def get_device_status(apic: APIC,
+                      devices: List[Something]) -> List[Dict[str, Something]]:
     """Upgrade status for any device type"""
     # TODO this should probably be threaded.
     # May take a while for a large fabric.
@@ -290,30 +284,33 @@ def get_device_status(apic, devices):  # type (APIC, dict) -> dict
         else:
             query_percent = round(100 * (i / device_count))
             inline_progress('Querying devices: %d%%', query_percent)
-    return result  # {device: ..., status: ..., running: ...}
+    return result  # List[{device: ..., status: ..., running: ...}]
 
 
-def parse_upgrade_state(apic, status):  # type: (APIC, list(dict)) -> str
+def parse_upgrade_state(apic: APIC, status: List[Dict[str, Something]]) -> str:
     """Parse and print upgrade status details."""
     scheduled_devices = []
     queued_devices = []
     upgrading_devices = []
+    unavailable_devices = []
     for device in status:
         # First determine how many devices are doing what...
         #
-        # status =
+        # device['status'] =
         # | scheduled - upgrade scheduled (target version != current version)
         # | inqueue - node waiting to upgrade, e.g. 20 device limit, etc
         # | inprogress - upgrade is happening now
         # | waitonbootup - waiting for device to reboot
         # | completeok - completed successfully
         # Note that this is not an exhaustive list of states
-        status = device['status']['upgradeStatus'].value()
-        if status == 'scheduled':
+        state = device['status']['upgradeStatus'].value()
+        if state == 'scheduled':
             scheduled_devices.append(device)
-        elif status == 'inqueue':
+        elif state == 'inqueue':
             queued_devices.append(device)
-        elif status != 'completeok':
+        elif state is None:
+            unavailable_devices.append(device)
+        elif state != 'completeok':
             upgrading_devices.append(device)
     if len(scheduled_devices) > 0:
         log_i('Status',
@@ -351,6 +348,17 @@ def parse_upgrade_state(apic, status):  # type: (APIC, list(dict)) -> str
                     device['status']['desiredVersion'].value(),
                     'Maintenance group': device['status']['maintGrp'].value(),
                 })
+    if len(unavailable_devices) > 0:
+        log_w(
+            'Status', '%d device(s) are not providing a current status.' %
+            len(unavailable_devices))
+        print(
+            'Devices may be rebooting due to upgrade or other maintenance work.'
+        )
+        for device in unavailable_devices:
+            ip = device['device']['address'].value()
+            name = device['device']['name'].value()
+            log_w('%s %s' % (name, ip), 'Unavailable.')
     if len(upgrading_devices) > 0:
         log_w('Status', '%d device(s) upgrading.' % len(upgrading_devices))
         percents = []
@@ -376,39 +384,76 @@ def parse_upgrade_state(apic, status):  # type: (APIC, list(dict)) -> str
         if len(percents) > 0:
             average_percent = round(sum(percents) / len(percents))
             log_i('Status', 'Average total percent: %d%%' % average_percent)
-    if len(queued_devices) == 0 and len(upgrading_devices) == 0:
+    if len(queued_devices) == 0 and len(upgrading_devices) == 0 and len(
+            unavailable_devices) == 0:
         log_i('Status', 'No devices currently undergoing upgrade.')
         return 'stable'
     return 'upgrading'
 
 
-def get_faults(apic):  # type: (APIC) -> list(Something)
+def get_faults(apic: APIC) -> List[Something]:
     """Get fault list w/ details."""
     faults = get(apic, '/api/class/faultInfo')
-    return [fault['faultInst']['attributes'] for fault in faults]
+    result = []
+    for fault in faults:
+        fault_attributes = fault['faultInst']['attributes']
+        if fault_attributes.value() is not None:
+            result.append(fault_attributes)
+    return result
 
 
-def load_snapshot(apic):  # type: (APIC) -> list(Something)
+def load_snapshot(apic: APIC) -> APIC:
     """Load/create snapshot"""
     fn = apic.options.snapshot
     faults = get_faults(apic)
     if not os.path.isfile(fn):
         with open(fn, 'w') as f:
             log_i('Snapshot', 'Creating new snapshot %s...' % fn)
-            f.write(json.dumps([fault.value() for fault in faults]))
-            return faults
-    with open(fn, 'r') as f:
-        log_i('Snapshot', 'Loading snapshot %s...' % fn)
-        return [Something(fault) for fault in json.loads(f.read())]
+            apic.faults = [fault['dn'].value() for fault in faults]
+            f.write(json.dumps(apic.faults, indent=2))
+    else:
+        with open(fn, 'r') as f:
+            log_i('Snapshot', 'Loading snapshot %s...' % fn)
+            apic.faults = [dn for dn in json.loads(f.read())]
+            #
+            # defensive file reading
+            if not isinstance(apic.faults, list):
+                apic.faults = []
+    return apic
 
 
-def request_loop(apic, devices):  # type: (APIC, list(Something)) -> None
+def check_faults(apic: APIC) -> None:
+    current_faults = get_faults(apic)
+    new_faults = []
+    for fault in current_faults:
+        dn = fault['dn'].value()
+        if dn not in apic.faults:
+            new_faults.append(fault)
+    if len(new_faults) > 0:
+        log_e('Fault Status',
+              '%s new fault(s) since previous snapshot.' % len(new_faults))
+        for fault in new_faults:
+            if apic.options.debug:
+                log_w('New Fault: %s' % fault['code'].value(), fault.value())
+            else:
+                log_w(
+                    'New Fault: %s' % fault['code'].value(), {
+                        'dn': fault['dn'].value(),
+                        'severity': fault['severity'].value(),
+                        'description': fault['description'].value(),
+                    })
+
+    else:
+        log_i('Fault Status', 'No new faults since snapshot.')
+
+
+def request_loop(apic: APIC, devices: List[Something]) -> None:
     """Monitor status."""
     while True:
         results = get_device_status(apic, devices)
         upgrade_state = parse_upgrade_state(apic, results)
         if upgrade_state == 'stable':
-            get_faults(apic, devices)
+            check_faults(apic)
         elapsed_time = round(time.time() - apic.start_time)
         if apic.options.verbose:
             log_i('Timer', 'Total run time: %d seconds.' % elapsed_time)
@@ -417,12 +462,12 @@ def request_loop(apic, devices):  # type: (APIC, list(Something)) -> None
         time.sleep(apic.options.request_interval)
 
 
-def main_loop(apic):  # type: (APIC) -> None
+def main_loop(apic: APIC) -> None:
     """Login and handle connecitivity exceptions."""
     devices = get_devices(apic)
     while True:
         try:
-            apic.login()
+            login(apic)
             request_loop(apic, devices)
         except ConnectionError:
             log_w('Connection Failed', 'Trying again...')
@@ -439,14 +484,13 @@ def main_loop(apic):  # type: (APIC) -> None
             print('Note: this is expected when the APIC first boots.')
 
 
-def main():  # type: () -> None
+def main() -> None:
     """Initial entry and top-level exception handling."""
     while True:
         try:
-            apic = login(APIC(get_options()))
-            #
-            #
-            #
+            apic = APIC(get_options())
+            apic = login(apic)
+            apic = load_snapshot(apic)
             main_loop(apic)
         except KeyboardInterrupt:
             exit()
@@ -459,7 +503,7 @@ def main():  # type: () -> None
                   'Please check your credentials and try again.')
             exit()
         except Exception as e:
-            # Run forever unless intentionally failing
+            # Run forever unless Ctrl-C, etc
             WAIT = 10
             log_e('Unexpected Error', str(e))
             traceback.print_exc()
